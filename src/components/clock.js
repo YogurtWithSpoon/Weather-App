2ab0dc49ecf49c5bf8d781f50dbe3262554bd097
Egor Kochkin <egurt96@yandex.ru>
2020-08-15 20:32:35 +0300

    change component name to Clock

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
-const IntervalExample = () => {
+const Clock = () => {
   const [time,setTime] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {setTime(() => {
-        console.log('hello')
         return `${new Date().getHours()}:${new Date().getMinutes()<10 ? "0"+new Date().getMinutes() : new Date().getMinutes()}`;
     })},1000);
     return () => clearInterval(interval);
@@ -16,4 +15,4 @@ const IntervalExample = () => {
   );
 };
 
-export default IntervalExample;
+export default Clock;
