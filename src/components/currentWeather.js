66aa75de3e647ccf64fcf07c29dcd1596e613178
Egor Kochkin <egurt96@yandex.ru>
2020-08-15 20:33:11 +0300

    Add converter of wind deg to word direction

--- src/components/currentWeather.js
+++ src/components/currentWeather.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { timeConverter, pressureConverter } from "../converter/converter";
+import { timeConverter, pressureConverter, windConverter } from "../converter/converter";
 
 const currentWeather = (props) => {
   const { data } = props;
@@ -15,7 +15,7 @@ const currentWeather = (props) => {
       {data.rain ? <p>{data.rain} мм</p> : null}
       {data.snow ? <p>{data.snow} мм</p> : null}
       <p>Ощущается как {Math.round(data.feels_like)} &#8451;</p>
-      <p>Ветер {} {data.wind_speed} м/с</p>
+      <p>Ветер {windConverter(Number(data.wind_deg))} {data.wind_speed} м/с</p>
       <p>Облачность {data.clouds} %</p>
       <p>Влажность {data.humidity} %</p>
       <p>Давление {pressureConverter(data.pressure)} мм. рт. ст.</p>
--- src/converter/converter.js
+++ src/converter/converter.js
@@ -1,40 +1,46 @@
-export function timeConverter(time){
-  let date = new Date(time*1000);
-  let minutes = date.getMinutes()>10 ? date.getMinutes() : "0"+date.getMinutes()
+export function timeConverter(time) {
+  let date = new Date(time * 1000);
+  let minutes = date.getMinutes() > 10 ? date.getMinutes() : "0" + date.getMinutes()
   let hours = date.getHours();
-  return hours+":"+minutes;
+  return hours + ":" + minutes;
 }
 
-export function pressureConverter(pressure){
-  return Math.floor(pressure/1.33);
+export function pressureConverter(pressure) {
+  return Math.floor(pressure / 1.33);
 }
 
-export function dateConverter(time){
-  return new Date(time*1000).getDate()+"/"+ (new Date(time*1000).getMonth()+1);
+export function dateConverter(time) {
+  return new Date(time * 1000).getDate() + "/" + (new Date(time * 1000).getMonth() + 1);
 }
 
-export function dayConverter(time){
-  return new Date(time*1000).getDay();
+export function dayConverter(time) {
+  return new Date(time * 1000).getDay();
 }
 
-export function windConverter(deg){
-  const dictonary = {
-  //   "С":348.75-11.25,
-  //   "":11.25 - 33.75,
-  //   NE:33.75 - 56.25,
-  //   ENE:56.25 - 78.75,
-  //   E:78.75 - 101.25,
-  //   ESE:101.25 - 123.75,
-  //   SE:123.75 - 146.25,
-  //   SSE:146.25 - 168.75,
-  //   S:168.75 - 191.25,
-  //   SSW:191.25 - 213.75,
-  //   SW:213.75 - 236.25,
-  //   WSW:236.25 - 258.75,
-  //   W:258.75 - 281.25,
-  //   WNW:281.25 - 303.75,
-  //   NW:303.75 - 326.25,
-  //   NNW:326.25 - 348.75,
+export function windConverter(deg) {
+  const dic = {
+    'С-СВ': [11.25,33.75],
+    'С-В': [33.75,56.25],
+    'В-СВ': [56.25,78.75],
+    'В': [78.75,101.25],
+    'В-ЮВ': [101.25,123.75],
+    'ЮВ': [123.75,146.25],
+    'Ю-ЮВ': [146.25,168.75],
+    'Ю': [168.75,191.25],
+    'Ю-ЮЗ': [191.25,213.75],
+    'ЮЗ': [213.75,236.25],
+    'З-ЮЗ': [236.25,258.75],
+    'З': [258.75,281.25],
+    'З-СЗ': [281.25,303.75],
+    'СЗ': [303.75,326.25],
+    'С-СЗ': [326.25,348.75],
+    // 'C': [348.75 - 11.25],
   }
+  for(let direction in dic){
+    if(deg > dic[direction][0] && deg < dic[direction][1]){
+      return direction
+    }
+  }
+  return "C";
 }
 
